4643253baf5bdbcd1032f95aa220fd60f2394815
Felix A. Schultz <felix.schultz@engapho.com>
2023-10-15 00:15:51 +0200

    Updated move

--- tetris.js
+++ tetris.js
@@ -110,61 +110,100 @@ class Tetris {
     }
 
     /* Update */
-    update(event) {
+    update(event = null) {
         /* Left */
-        if (event.keyCode === 37) {
+        if (event != null && event.keyCode === 37) {
             if(this.player.pos.x > 0){
-                this.player.pos.x--;
+                this.player.pos.x-1;
             }
         }
         
         /* Right */
-        if (event.keyCode === 39) {
+        if (event != null && event.keyCode === 39) {
             if(this.player.pos.x < this.canvas.width  / 10 - 3){
-                this.player.pos.x++;
+                this.player.pos.x+1;
             }
         }
         
         /* Down */
-        if (event.keyCode === 40) {
-            if(this.player.pos.y / 10 - 1 < this.canvas.height){
-                this.player.pos.y++;
-            }
+        if(this.player.pos.y / 10 - 1 < this.canvas.height){
+            this.player.pos.y+1;
+        }
 
-            if(this.player.pos.y === this.canvas.height / 10 - 1){
-                const currentMatrix = this.player.matrix;
-                const currentMatrixArray = localStorage.getItem('tetris') === null ? [] : JSON.parse(localStorage.getItem('tetris'));
-                currentMatrixArray.push(currentMatrix);
-                localStorage.setItem('tetris', JSON.stringify(currentMatrixArray));
-                this.player.matrix = this.createPiece(this.types[Math.floor(Math.random() * this.types.length)]);
-                this.player.pos.x = 10;
-                this.player.pos.y = 0;
-                this.run();
+        if(this.player.pos.y / 10 - 1 == this.canvas.height){
+            this.collision();
+        }
+
+        if(this.player.pos.y == this.canvas.height / 10 - 1){
+            /* this.player.matrix = this.createPiece(this.types[Math.floor(Math.random() * this.types.length)]) */
+        }
+        
+
+    }
+
+    /* Fall */
+    move(event) {
+        if(event.keyCode === 40){
+            if(this.player.pos.y != this.canvas.height / 10 - 1){
+                this.player.pos.y++;
             }
         }
 
-        /* Keep tetris piece at the bottom */
-        this.keepTetrisBlocksAtTheBottomOfCanvas();
+        if(event.keyCode === 38){
+            this.player.pos.y--;
+        }
 
+        if(event.keyCode === 37){
+            if(this.player.pos.x > 0){
+                this.player.pos.x--;
+            }
+
+            /* no move when brick collided with either the bottom of the canvas or another block */
+            if(this.player.pos.x == 0){
+                this.player.pos.x = 0;
+            }
+
+        }
+
+
+
+        if(event.keyCode === 39){
+            if(this.player.pos.x < this.canvas.width / 10 - 3){
+                this.player.pos.x++;
+            }
+
+            /* no move when brick collided with either the bottom of the canvas or another block */
+            if(this.player.pos.x == this.canvas.width / 10 - 3){
+                this.player.pos.x = this.canvas.width / 10 - 3;
+            }
+
+        }
     }
 
     /* Run */
     run() {
+        this.update();
         this.draw();
         requestAnimationFrame(() => this.run());
     }
 
-    /* Keep Tetris blocks at the bottom of canvas */
-    keepTetrisBlocksAtTheBottomOfCanvas() {
-        JSON.parse(localStorage.getItem('tetris')).forEach((row, y) => {
-            row.forEach((value, x) => {
-                if (value !== 0) {
-                    this.context.fillStyle = "#FF0000";
-                    this.context.fillRect(x + this.player.pos.x, y + this.player.pos.y, 1, 1);
-                }
-            });
-        })
+    /* Save */
+    save() {
+        const currentMatrix = {
+            matrix: this.player.matrix,
+            pos: this.player.pos
+        };
+        const currentMatrixArray = localStorage.getItem('tetris') === null ? [] : JSON.parse(localStorage.getItem('tetris'));
+        currentMatrixArray.push(currentMatrix);
+        localStorage.setItem('tetris', JSON.stringify(currentMatrixArray));
     }
+
+    /* Collison */
+    collision() {
+        
+    }
+
+    /*  */
 
 
     /* Start */
@@ -178,5 +217,5 @@ const tetris = new Tetris();
 tetris.start();
 
 document.addEventListener('keydown', event => {
-    tetris.update(event);
+    tetris.move(event);
 });
